c95a52b4857d91f7fa38b820a9c0de1d5b8041ff
Muhammad Aditia Syauqi <syauqi.aditia@gmail.com>
2021-02-18 21:05:21 +0700

    Aplikasi

---
+++ Aplikasi/flutter_application_1/lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const primaryColor = Color(0xffffffff);
+  static const primaryDarkColor = Color(0xff607Cbf);
+  static const underlineTextField = Color(0xff081530);
+  static const hintColor = Color(0xff0c1832);
+}
